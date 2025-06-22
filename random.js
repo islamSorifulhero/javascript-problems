3025767e7d42225463f674706511734dd6f52bbc
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-06-22 08:52:59 +0600

    my first commit

---
+++ random.js
@@ -0,0 +1,22 @@
+// var num = 2.449292;
+// var result = Math.floor(num);
+// console.log(result);
+
+// var num = 2.33894;
+// var result1 = Math.ceil(num);
+// console.log(result1);
+
+// var num = 2.58484;
+// var result2 = Math.round(num);
+// console.log(result2)
+
+// var num = 3.38484;
+// var result3 = Math.random(num) * 10;
+// console.log(result3)
+
+var num = 3.333;
+for(var i = 0; i <10; i++) {
+    var result4 = Math.random(num) * 6;
+    var output = Math.round(result4)
+    console.log(output)
+}
